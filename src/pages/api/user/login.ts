4d91eebc28b4c2605d7a67a4f1a26a1908dfa106
Julie Souchet <jsouchet@easyvista.com>
2021-12-10 15:55:04 +0100

    [project] Rename and refactor for better tree structure

--- src/pages/api/user/login.ts
+++ src/pages/api/user/login.ts
@@ -1,32 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { login } from '../../../server/services/users/users.service';
+import { defaultHandler } from '../../../server/common/default.handler';
+import { loginPostHandler } from '../../../server/handlers/user/login/post.handler';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case 'POST':
-      await postRoute(req, res);
-      break;
-    default:
-      res.status(501).send('Not implemented');
-  }
-};
-
-const postRoute = async (req: NextApiRequest, res: NextApiResponse) => {
-  const user = req.body;
-  if (!user.password || !user.email) {
-    res.status(400).json({error: 'Veuillez remplir le formulaire.'});
+  const handlers = {
+    "POST": loginPostHandler,
+    // add here handlers for other methods
   }
 
-  const result = await login(user);
-  if (result == true) {
-    res.status(200).json({
-      success: true
-    });
-  }
-  else {
-    res.status(400).json({
-      success: false,
-      error: result['error'] || 'Login failed'
-    });
-  }
+  const handler = handlers[req.method] || defaultHandler;
+
+  handler(req, res);
 };
