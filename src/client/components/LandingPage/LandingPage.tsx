0754b0dbb063c6fc7a18edfd7e6b0614018c1b57
Allan <allan.duhe@edu.devinci.fr>
2021-12-10 16:59:16 +0100

    [lp] wip

--- src/client/components/LandingPage/LandingPage.tsx
+++ src/client/components/LandingPage/LandingPage.tsx
@@ -1,17 +1,13 @@
 import { FunctionComponent } from "react";
 import styles from '../../styles/LandingPage/LandingPage.module.scss';
-import ZoneOne from "./ZoneOne";
-import ZoneTwo from "./ZoneTwo";
-import ZoneThree from "./ZoneThree";
+
 
 interface Props {}
 
 const LandingPage: FunctionComponent<Props> = () => {
   return (
     <div className={styles.container}>
-      <ZoneOne/>
-      <ZoneTwo/>
-      <ZoneThree/>
+
     </div>
   );
 };
